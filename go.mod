5b87ef38d777070c484ec4756e5a02c0873e84c9
ozx1812 <om.mkp1812@yahoo.com>
2023-06-29 12:22:31 -0400

    init go-automation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ozx1812/go-automation
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Print("Go Automation for Kubernetes deployments...")
+}
